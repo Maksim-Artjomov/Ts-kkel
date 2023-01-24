435dc0f60c3dd677e0e0a5de3d754889890e9426
kakish <kakish@MAKS>
2023-01-25 01:07:47 +0300

    Добавьте файлы проекта.

---
+++ Tsükkel/Tsükkel.py
@@ -0,0 +1,47 @@
+﻿##print("Ülesanne 0-1")
+
+#mybool=True
+#i=(int)
+#while mybool:
+#    print("please enter 72: ")
+#    i=(int(input("Sisestage täisarv => ")))
+#    if i == 72: print("Thanks");mybool=False
+
+##print("Ülesanne 0-2") 
+
+#for i in range (60,73):
+#    print (i)
+#    print("please wait for 72 ")
+#    if i == 72: print("Thanks");mybool=False
+
+
+#=====================================================================================================
+##print("Ülesanne 23")  
+
+#Vanus={}
+#for i in range(1,20):
+#    print(f"Õpilase number {i}, sisestage oma vanus: ")
+#    a = list(Vanus)
+#    a.append(int(input()))
+#    Vanus = tuple(a)
+#Vanus = tuple(sorted(Vanus))
+
+#if (Vanus[0])<=17:
+#    print("Selles klassis on alaealisi vanuses, näiteks: "+str(Vanus[0]))
+#if (Vanus[1])<=17:
+#    print("Selles klassis on alaealisi vanuses, näiteks: "+str(Vanus[1]))
+#if (Vanus[2])<=17:
+#    print("Selles klassis on alaealisi vanuses, näiteks: "+str(Vanus[2]))
+#if (Vanus[0])>=18:
+#    print("Selles klassis ei ole alaealisi.")
+
+
+#if (Vanus[-3])>=18:
+#    print("Sellel klassil on täiskasvanud vanuses, näiteks: "+str(Vanus[-3]))
+#if (Vanus[-2])>=18:
+#    print("Sellel klassil on täiskasvanud vanuses, näiteks: "+str(Vanus[-2]))
+#if (Vanus[-1])>=18:
+#    print("Sellel klassil on täiskasvanud vanuses, näiteks: "+str(Vanus[-1]))
+#if (Vanus[-1])<=17:
+#    print("Selles klassis pole täiskasvanuid.")
+##print(Vanus)
